36a0b365c6769ae3f2fa84af53c0a6e72a47884e
luo <1240564638@qq.com>
2023-06-26 16:25:44 +0800

    重置

--- xjdzy_backend/src/main/java/com/xjdzy/mapper/ArticleImagesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xjdzy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xjdzy.entity.ArticleImages;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ArticleImagesMapper extends BaseMapper<ArticleImages> {
-}
--- xjdzy_backend/src/main/java/com/xjdzy/mapper/LikesMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xjdzy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xjdzy.entity.Likes;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LikesMapper extends BaseMapper<Likes> {
-}
